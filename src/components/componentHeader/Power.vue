7fb37aeadaea2540c9ab34f502ec51df4fadb3f8
valeria caramellino <valeriacaramellino@gmail.com>
2024-01-16 16:05:11 +0100

    hover

--- src/components/componentHeader/Power.vue
+++ src/components/componentHeader/Power.vue
@@ -1,15 +1,14 @@
 <script>
-export default{
-    name:'Power',
-    props:{
-        classe:String
+export default {
+    name: 'Power',
+    props: {
+        classe: String
     }
 }
 </script>
 <template>
-    
     <div class="col-2 my-2 d-flex justify-content-center align-items-center py-2">
-                
+
         <div class="col-11 d-flex justify-content-center align-items-center ">
 
             <i :class="classe"></i>
@@ -20,15 +19,23 @@ export default{
 </template>
 <style lang="scss" scoped>
 .col-2 {
-        .col-11 {
-            border: 1px solid black;
-            width: 70px;
-            height: 70px;
-            border-radius: 20px;
 
+    .col-11 {
+        border: 1px solid black;
+        width: 70px;
+        height: 70px;
+        border-radius: 20px;
+
+        i {
+            font-size: 30px;
+        }
+
+        &:hover {
             i {
-                font-size: 30px;
+                color: darkorange;
+                cursor: pointer;
             }
         }
     }
+}
 </style>
