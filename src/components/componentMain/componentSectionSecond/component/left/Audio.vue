f908e77d2a54bd47d1910aa13486e8fdf94391b8
valeria caramellino <valeriacaramellino@gmail.com>
2024-01-16 16:05:29 +0100

    fix righe verticali e hover

--- src/components/componentMain/componentSectionSecond/component/left/Audio.vue
+++ src/components/componentMain/componentSectionSecond/component/left/Audio.vue
@@ -1,45 +1,60 @@
 <script>
-export default{
-    name:'Audio'
+export default {
+    name: 'Audio'
 }
 </script>
 <template>
-    <section class="d-flex align-items-center justify-content-start">
+    <section class="d-flex justify-content-center align-items-baseline">
         <div class="volume d-flex justify-content-center align-items-center mx-3">
 
             <i class="fa-solid fa-volume-low "></i>
+
+        </div>
+        <div class="mx-3 lines-container ">
+            <span class="line">||</span>
             
         </div>
-        <div class="mx-3">
-            
-            <span v-for="index in 4" :key="index"><i class="fa-solid fa-grip-lines-vertical"></i></span>
-        </div>
-        
+
     </section>
 </template>
 <style lang="scss" scoped>
-div.volume{
+
+.volume {
     width: 60px;
     height: 60px;
     border: 2px solid black;
     border-radius: 20px;
-    //background-color: blue;
-    i{
-        padding: 1rem;
-        font-size: 25px;
+    i {
+      padding: 1rem;
+      font-size: 25px;
     }
-}
-div{
-//background-color: blueviolet;
-width: 80px;
-height: 60px;
-display: flex;
-align-items: center;
-justify-content: space-around;
-    span{
-        
+    &:hover{
+        i{
+            cursor: pointer;
+            color: darkorange;
+        }
+    }
+  }
+
+  .lines-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    .line {
+      font-size: 50px;
+   
+      &::before {
         font-size: 40px;
-        font-weight: 600;
+        content: "||";
+        margin-right: 5px;
+      }
+
+      &::after {
+        font-size: 40px;
+        content: "||";
+        margin-left: 5px;  
+      }
     }
-}
+  }
 </style>
