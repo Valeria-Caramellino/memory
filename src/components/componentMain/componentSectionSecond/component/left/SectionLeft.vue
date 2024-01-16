29b94f3a6569db5527a0050eaba25cbddde91336
valeria caramellino <valeriacaramellino@gmail.com>
2024-01-16 15:36:35 +0100

    parti della seconda sezione

---
+++ src/components/componentMain/componentSectionSecond/component/left/SectionLeft.vue
@@ -0,0 +1,24 @@
+<script>
+import Audio from './audio.vue';
+import Levels from './Levels.vue';
+export default{
+    name:'Left',
+    components:{
+        Audio,
+        Levels
+    }
+}
+</script>
+<template>
+    <div class="col-3 d-flex flex-column justify-content-around align-items-center mx-3">
+        <Audio/>
+        <Levels/>
+
+    </div>
+</template>
+<style lang="scss" scoped>
+.col-3{
+    border: 2px solid black;
+    border-radius: 10px;
+}
+</style>
---
+++ src/components/componentMain/componentSectionSecond/component/right/SectionRight.vue
@@ -0,0 +1,18 @@
+<script>
+import Simbols from './Simbols.vue';
+export default{
+    name:'Right',
+    components:{
+        Simbols
+    }
+}
+</script>
+<template>
+    <div class="col-8">
+        <Simbols/>
+    </div>
+</template>
+<style lang="scss" scoped>
+
+
+</style>
