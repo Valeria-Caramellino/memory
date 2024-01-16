b7bfb164d4eacc40b485a919713938ebbdfaaff7
valeria caramellino <valeriacaramellino@gmail.com>
2024-01-16 10:28:26 +0100

    right cube

---
+++ src/components/RightCube.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    name:'RightCube'
+}
+
+</script>
+<template>
+
+    <section class="col-6 d-flex justify-content-center align-items-center">
+        <h1>Cubo Destro</h1>
+    </section>
+</template>
+<style lang="scss" scoped>
+section{
+    //width: 400px;
+    height: 200px;
+    background-color: black;
+    border-radius: 10px;
+    h1{
+        color: orange;
+    }
+}
+</style>
