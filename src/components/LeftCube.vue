f027c1e91ad9740546ced512c69a5ad1821e35e1
valeria caramellino <valeriacaramellino@gmail.com>
2024-01-16 10:28:19 +0100

    left cube

---
+++ src/components/LeftCube.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    name:'LeftCube'
+}
+
+</script>
+<template>
+    <section class="col-3 d-flex justify-content-center align-items-center">
+
+        <img src="/smile.gif" alt="gif">
+        
+    </section>
+</template>
+<style lang="scss" scoped>
+section{
+    //width: 200px;
+    height: 200px;
+    background-color: black;
+    border-radius: 10px;
+    img{
+        height: 100%;
+    }
+}
+
+</style>
